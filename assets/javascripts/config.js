286fc364fbb83c2a0cfdb043fe76d0d025d75671
Ontnanda <Ontnanda@192.168.1.48>
2017-05-24 17:16:44 +0700

    change config for fix conflic

--- assets/javascripts/config.js
+++ assets/javascripts/config.js
@@ -1,8 +1,3 @@
-var molConfig = {
-  centerThai: [11302896.246585583, 1477374.8826958865],
-  zoomThai: 6,
-  mode: "normal",
-  currentLayer: null,
-  drawColor: "#ffcc33",
-  geoserverUrl: "http://192.168.0.202:8080/geoserver",
-};
+var config = {
+  geoserverUrl: 'http://192.168.0.241:8080/geoserver'
+}
