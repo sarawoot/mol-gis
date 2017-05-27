d0b9e34733b01c168124c2ab54e2f9bd71cd5a5a
Ontnanda <Ontnanda@192.168.1.48>
2017-05-27 17:03:05 +0700

    xx

--- assets/javascripts/config.js
+++ assets/javascripts/config.js
@@ -1,5 +1,5 @@
 var config = {
-  geoserverUrl: 'http://192.168.0.241:8080/geoserver'
+  geoserverUrl : 'http://192.168.0.241:8080/geoserver'
 }
 
-var noFormHaveAmphor = [];
+var noFormHaveAmphor = [ 1, 2, 3, 4, 5, 6, 8, 9, 10, 11, 12, 13 ];
--- helpers/search.php
+++ helpers/search.php
@@ -1,55 +1,100 @@
 <?php
-function genSql(){
+function genSql($param){
   $sql = '';
-  $_GET['form'] = ( int ) $_GET['form'];
+  $param['formSearch'] = ( int ) $param['formSearch'];
   $tbl = '';
-  switch($_GET['form']){
+  switch($param['formSearch']){
+    case 2 :
+      $tbl = 'VIEW_GIS_DSD_PASS_TRAINING';
+      $sum_field = 'PASS_TRAIN_AMT';
+      break;
+    case 3 :
+      $tbl = 'VIEW_GIS_DSD_PASS_TESTING';
+      $sum_field = 'PASS_TEST_AMT';
+      break;
+    case 4 :
+      $tbl = 'VIEW_GIS_DOE_JOB_VACANCY';
+      $sum_field = 'VACANCY_AMT';
+      break;
+    case 5 :
+      $tbl = 'VIEW_GIS_SSO_INSURED_M33';
+      $sum_field = 'M33_AMT';
+      break;
+    case 6 :
+      $tbl = 'VIEW_GIS_SSO_INSURED_M40';
+      $sum_field = 'M40_AMT';
+      break;
     case 8 :
       $tbl = 'VIEW_GIS_STAT_NSO_DISABILITY';
+      $sum_field = 'WEIGHT_AMT';
       break;
     case 9 :
       $tbl = 'VIEW_GIS_STAT_NSO_ELDER';
+      $sum_field = 'WEIGHT_AMT';
       break;
     case 10 :
       $tbl = 'VIEW_GIS_STAT_NSO_MONTHLY';
+      $sum_field = 'WEIGHT_AMT';
       break;
     case 11 :
       $tbl = 'VIEW_GIS_STAT_NSO_QUARTER';
+      $sum_field = 'WEIGHT_AMT';
       break;
     case 12 :
       $tbl = 'VIEW_GIS_STAT_NSO_INFORMAL_WK';
+      $sum_field = 'WEIGHT_AMT';
       break;
+    case 13 :
+      $tbl = 'VIEW_GIS_DOE_FOREIGN_WORKER';
+      $sum_field = 'FPREIGN_AMT';
+      break;
+    
     default :
       $tbl = 'VIEW_GIS_STAT_NSO_DISABILITY';
+      $sum_field = 'WEIGHT_AMT';
       break;
   }
+  $form_array = [
+      2,3,4,5,6,7,8,9,10,11,12,13
+  ];
   
-  switch($_GET['form']){
-    case 8 :
-    case 9 :
-    case 10 :
-    case 11 :
-    case 12 :
+  switch(true){
+    case (in_array($form_array, $param['formSearch'])) :
     default :
-      $sql = " SELECT  sum(WEIGHT_AMT)cnt ,CWT_CODE,CWT_DESC FROM $tbl WHERE 1=1 ";
+      $sql = " SELECT  sum($sum_field)cnt ,CWT_CODE,CWT_DESC FROM $tbl WHERE 1=1 ";
       
-      if (isset($_GET["YEAR_TH"])){
-        $sql .= " AND YEAR_TH = '" . ( int ) $_GET["YEAR_TH"] . "'";
+      if (isset($param["FOREIGN_TYPE_CODE"])){
+        $sql .= " AND FOREIGN_TYPE_CODE = '" . replace_str($param["FOREIGN_TYPE_CODE"]) . "'";
       }
-      if (isset($_GET["DISABILITY_GROUP_CODE"])){
-        $sql .= " AND DISABILITY_GROUP_CODE = '" . replace_str($_GET["DISABILITY_GROUP_CODE"]) . "'";
-      }
-      
-      if (isset($_GET["DISABILITY_TYPE_CODE"])){
-        $sql .= " AND DISABILITY_TYPE_CODE = '" . replace_str($_GET["DISABILITY_TYPE_CODE"]) . "'";
+      if (isset($param["LAW_OCCUPATION_CODE"])){
+        $sql .= " AND LAW_OCCUPATION_CODE = '" . ( int ) $param["LAW_OCCUPATION_CODE"] . "'";
       }
       
-      if (isset($_GET["MONTH_CODE"])){
-        $sql .= " AND MONTH_CODE = " . ( int ) $_GET["MONTH_CODE"];
+      if (isset($param["TRAIN_ACTIVITY_CODE"])){
+        $sql .= " AND TRAIN_ACTIVITY_CODE = '" . ( int ) $param["TRAIN_ACTIVITY_CODE"] . "'";
       }
       
-      if (isset($_GET["QUARTER"])){
-        $sql .= " AND QUARTER = " . ( int ) $_GET["QUARTER"];
+      if (isset($param["CWT_CODE"])){
+        $sql .= " AND CWT_CODE = '" . ( int ) $param["CWT_CODE"] . "'";
+      }
+      
+      if (isset($param["YEAR_TH"])){
+        $sql .= " AND YEAR_TH = '" . ( int ) $param["YEAR_TH"] . "'";
+      }
+      if (isset($param["DISABILITY_GROUP_CODE"])){
+        $sql .= " AND DISABILITY_GROUP_CODE = '" . replace_str($param["DISABILITY_GROUP_CODE"]) . "'";
+      }
+      
+      if (isset($param["DISABILITY_TYPE_CODE"])){
+        $sql .= " AND DISABILITY_TYPE_CODE = '" . replace_str($param["DISABILITY_TYPE_CODE"]) . "'";
+      }
+      
+      if (isset($param["MONTH_CODE"])){
+        $sql .= " AND MONTH_CODE = " . ( int ) $param["MONTH_CODE"];
+      }
+      
+      if (isset($param["QUARTER"])){
+        $sql .= " AND QUARTER = " . ( int ) $param["QUARTER"];
       }
       
       $sql .= " GROUP BY CWT_CODE,CWT_DESC";
